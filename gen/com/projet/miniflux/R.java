645d7a715a6f947d663a7a0b49b433a09f9b2907
rosesoub <rosesoub@192.168.20.160>
2014-04-23 19:12:54 +0200

    Test Authentification 1 : je ne suis pas sûre que ça marche. Quand il faudra regrouper les 2 projets utilise plutôt d'abord l'enseigne versions.

--- gen/com/projet/miniflux/R.java
+++ gen/com/projet/miniflux/R.java
@@ -57,19 +57,19 @@ public final class R {
     }
     public static final class string {
         public static final int action_favorites=0x7f050002;
-        public static final int action_refresh=0x7f05000d;
+        public static final int action_refresh=0x7f05000e;
         public static final int action_settings=0x7f050001;
-        public static final int add_feed=0x7f05000c;
+        public static final int add_feed=0x7f05000d;
         public static final int app_name=0x7f050000;
-        public static final int cancel=0x7f050011;
-        public static final int confirm=0x7f050009;
+        public static final int cancel=0x7f050012;
+        public static final int confirm=0x7f05000a;
         /**  activity_home 
          */
-        public static final int home_label=0x7f05000a;
-        public static final int link=0x7f05000f;
+        public static final int home_label=0x7f05000b;
+        public static final int link=0x7f050010;
         public static final int logout=0x7f050003;
         public static final int password=0x7f050007;
-        public static final int register=0x7f050010;
+        public static final int register=0x7f050011;
         /**  activity_main 
          */
         public static final int sign_in=0x7f050004;
@@ -77,10 +77,11 @@ public final class R {
         /**   DisplayMessageActivity
   <string name="edit_message">Edit</string>  
          */
-        public static final int subscription=0x7f050008;
-        public static final int text_in_case_feed_list_empty=0x7f05000b;
+        public static final int subscription=0x7f050009;
+        public static final int text_in_case_feed_list_empty=0x7f05000c;
         public static final int username=0x7f050006;
-        public static final int website=0x7f05000e;
+        public static final int website=0x7f05000f;
+        public static final int webview=0x7f050008;
     }
     public static final class style {
         /** 
